983579db92a5540342735b0af58f4d4dac566485
Mqr1o_ <144613794+Mqr1oo@users.noreply.github.com>
2026-03-15 13:46:32 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'glyph-v2';
+const ASSETS = [
+  './glyph.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
